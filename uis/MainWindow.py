41a24d67e6996b922aa129dcff59787b8b81709f
Bart Kolada <r3dn0w@gmailc.om>
2014-03-22 22:58:21 +0100

    Connected open epub and annot buttons some content in annot handler added log function

--- uis/MainWindow.py
+++ uis/MainWindow.py
@@ -6,26 +6,42 @@ import os
 from PyQt4 import QtGui
 
 from compiled import Ui_MainWindow
+from handlers import AnnotationHandler
+import WindowErrors as we
+import SupportingFunctions as sf
 
 class Controller(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QWidget.__init__(self, None)
-
         self.setup_ui()
+        self.clear_fields()
+
+    def clear_fields(self):
+        self.annot_handler = AnnotationHandler()
+
 
     def setup_ui(self):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.connect_buttons()
 
-    def connect__buttons(self):
-        pass
+    def connect_buttons(self):
+        self.ui.open_annot.clicked.connect(self.open_annot)
+        self.ui.open_epub.clicked.connect(self.open_epub)
 
     def open_epub(self):
         pass
 
     def open_annot(self):
-        pass
+        try:
+            file = sf.open_annot_window(self)
+            self.log(file)
+            self.annot_handler
+        except we.FileNotSelected as msg:
+            self.log(msg)
 
     def save_epub(self):
         pass
 
+    def log(self, log):
+        print log
