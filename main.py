a158ec66c17fbf7e40074eb0bbdd01767113654f
Bart Kolada <r3dn0w@gmailc.om>
2014-03-22 22:56:32 +0100

    Solved problem with relational pckg paths

--- main.py
+++ main.py
@@ -3,10 +3,13 @@
 import sys
 from PyQt4 import QtGui
 from uis import Controller
+import sys
+import os
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 if __name__ == '__main__':
+    sys.path.append(os.path.join(os.path.dirname(__file__)))
     app = QtGui.QApplication(sys.argv)
     app.setStyle(QtGui.QStyleFactory.create('Plastique'))
     main_app = Controller()
